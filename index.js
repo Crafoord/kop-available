50e251ce9c52bdfe0e1f6787836aa80e506b3ff2
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-01-18 16:57:00 +0100

    fail

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ const start = () => {
     if (err) exit();
     if (value === 1) {
       blinkLED();
-      console.log(new Date().toTimeString, "Someone is playing! ");
+      console.log(new Date().toTimeString(), "Someone is playing! ");
     } else {
       endBlink();
-      console.log(new Date().toTimeString, "Ping pong is available!");
+      console.log(new Date().toTimeString(), "Ping pong is available!");
     }
   });
 };
