9d59201935b6f55cb7250ca0fec0cd0f6c5c9c60
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-01-18 14:44:35 +0100

    log stuff

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const endBlink = () => {
   //function to stop blinking
   clearInterval(blinkInterval); // Stop blink intervals
   LED.writeSync(0); // Turn LED off
+  console.log("end blink, interval: ", blinkInteraval);
+  console.log("led state: ", LED.readSync());
 };
 
 const exit = () => {
