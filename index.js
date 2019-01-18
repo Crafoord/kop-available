1e3c6562c43a3d7889a3bad8b8096ebf343f0a63
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-01-18 14:38:53 +0100

    rearrange

--- index.js
+++ index.js
@@ -3,29 +3,9 @@ var PIR = new Gpio(17, "in", "both");
 var LED = new Gpio(4, "out");
 let blinkInterval;
 
-start();
-
-const start = () => {
-  PIR.watch((err, value) => {
-    if (err) exit();
-    alarm();
-    console.log("Intruder detected");
-    console.log(value);
-  });
-};
-
 const alarm = () => {
   this.blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
   setTimeout(endBlink, 5000); //stop blinking after 5 seconds
-};
-
-const blinkLED = () => {
-  if (LED.readSync() === 0) {
-    //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
 };
 
 const endBlink = () => {
@@ -40,3 +20,23 @@ const exit = () => {
   PIR.unexport();
   process.exit();
 };
+
+const blinkLED = () => {
+  if (LED.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+};
+
+const start = () => {
+  PIR.watch((err, value) => {
+    if (err) exit();
+    alarm();
+    console.log("Intruder detected");
+    console.log(value);
+  });
+};
+
+start();
