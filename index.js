46a1dbcc861abef7d9760ad8addbea7a3bc539c8
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-02-01 14:47:04 +0100

    add socket and endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Gpio = require("onoff").Gpio;
 var PIR = new Gpio(17, "in", "both");
+var URL = require("./secrets.js").URL;
+var socket = require("socket.io-client")(URL);
 
 const exit = () => {
   // Unexport GPIO to free resources
@@ -12,8 +14,10 @@ const start = () => {
   PIR.watch((err, value) => {
     if (err) exit();
     if (value === 1) {
+      socket.emit("available", false);
       console.log(new Date().toTimeString(), "Someone is playing! ");
     } else {
+      socket.emit("available", true);
       console.log(new Date().toTimeString(), "Ping pong is available!");
     }
   });
---
+++ secrets-example.js
@@ -0,0 +1,1 @@
+export const URL = "url-to-socket";
